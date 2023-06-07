6f94834710e72aba180b71ed01a0d5143ae81750
Gideon Gyebi <gyebigideon000@gmail.com>
2023-06-07 15:49:42 -0700

    fixing bug, printing new line after each letter (wrong)

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,4 @@ def uppercase(str):
     for i in str:
         if ord("a") <= ord(i) <= ord("z"):
             i = chr(ord(i) - 32)
-    print("{:s}".format(i))
-    print()
+            print("{:s}".format(i), end="")
