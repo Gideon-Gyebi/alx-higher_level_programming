b7a3d14a82841bfb4e22a77b214c434062356c00
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2023-06-08 00:00:49 +0000

    15. Remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def remove_char_at(str, n):
+    """Creating a copy of a string
+    without the character at "n" position."""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
