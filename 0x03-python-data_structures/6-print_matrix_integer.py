df3d2b84a89b6b8adbef71cbf7b6fe45f6e441b6
Gideon Gyebi <gyebigideon000@gmail.com>
2023-06-11 13:57:39 -0700

    Task: 6. Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-Printing a matrix of integers.
-"""
 def print_matrix_integer(matrix=[[]]):
+    """Printing a matrix of integers"""
     for row in matrix:
         for col in row:
             print("{:d}".format(col), end=" " if col != row[-1] else "")
