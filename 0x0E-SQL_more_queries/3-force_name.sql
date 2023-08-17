d310bd3ee5ec57bfa24b8bff6d032e9628886107
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2023-08-17 00:22:04 +0000

    Update 3. Always a name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- Creating the table force_name on your MySQL server.
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
