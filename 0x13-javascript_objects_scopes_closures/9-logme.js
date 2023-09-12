85fe41c3271251e255030e2b520e2238ec7a7106
Gideon-Gyebi <gyebigideon000@gmail.com>
2023-09-12 23:44:15 +0000

    9. Log me

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* A function that prints the number of arguments already printed 
+and the new argument value. */
+let narg = 0;
+
+exports.logMe = function (item) {
+  console.log(narg + ': ' + item);
+  narg++;
+};
