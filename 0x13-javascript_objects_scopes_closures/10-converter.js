399e2c7687f860e2ab4d253ce93d4468d7830e2c
Gideon-Gyebi <gyebigideon000@gmail.com>
2023-09-13 00:05:49 +0000

    debugging

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* A function that converts a number from base 10 
+/* A function that converts a number from base 10
 to another base passed as argument */
 
 exports.converter = function (base) {
