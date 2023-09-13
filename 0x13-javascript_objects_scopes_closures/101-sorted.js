79146c190dccc5f34e5f7e01026256dd3f682d53
Gideon-Gyebi <gyebigideon000@gmail.com>
2023-09-13 00:07:11 +0000

    debugging

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* A script that imports a dictionary of occurrences by user id 
+/* A script that imports a dictionary of occurrences by user id
 and computes a dictionary of user ids by occurrence. */
 const dict = require('./101-data').dict;
 
