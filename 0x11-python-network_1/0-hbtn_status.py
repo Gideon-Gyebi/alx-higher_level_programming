afa8bb011f24b61d9773f311f67911ad3137a89d
Gideon Gyebi <gyebigideon000@gmail.com>
2023-09-29 16:51:23 -0700

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A script that fetches this https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+def main():
+    """
+    This prints a response of the url
+    """
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf8')))
+
+if __name__ == "__main__":
+    main()
